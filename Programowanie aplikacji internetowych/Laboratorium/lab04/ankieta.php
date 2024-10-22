98888c2bd0c255b176eda936564a783f222a93e5
Mateusz Kierepka <mateusz.kierepka03@gmail.com>
2024-10-22 23:20:55 +0200

    Update to university-part-v

--- Programowanie aplikacji internetowych/Laboratorium/lab04/ankieta.php
+++ Programowanie aplikacji internetowych/Laboratorium/lab04/ankieta.php
@@ -1,32 +1,89 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ankieta</title>
-    <link rel="stylesheet" href="formularz.css" type="text/css">
-</head>
-<body>
-<h3>Wybierz technologie, które znasz:</h3>
-<form method="post" action="ankieta.php">
-
-    <?php
-    $tech = ["C", "CPP", "Java", "C#", "Html", "CSS", "XML", "PHP", "JavaScript"];
-    foreach ($tech as $t) {
-        echo "<input type='checkbox' name='$t' id='$t'>";
-        echo "<label for='$t'>$t</label><br>";
-    }
-    ?>
-
-    <input type="submit" value="Wyślij">
-</form>
-</body>
-</html>
-
 <?php
-    if (!(file_exists("ankieta.txt"))) {
-        $plik = fopen("ankieta.txt", "w+");
-        // uzupełnić
-        fclose($plik, "r+");
+
+$tech = ["C", "CPP", "Java", "C#", "Html", "CSS", "XML", "PHP", "JavaScript"];
+
+function wczytajWyniki($tech) {
+    $wyniki = [];
+    if (file_exists('ankieta.txt')) {
+        $lines = file('ankieta.txt', FILE_IGNORE_NEW_LINES);
+        foreach ($lines as $line) {
+            $parts = explode(':', $line);
+            if (count($parts) == 2) {
+                $wyniki[$parts[0]] = intval($parts[1]);
+            }
+        }
     }
+    foreach ($tech as $t) {
+        if (!isset($wyniki[$t])) {
+            $wyniki[$t] = 0;
+        }
+    }
+    return $wyniki;
+}
+
+function zapiszWyniki($wyniki) {
+    $content = '';
+    foreach ($wyniki as $tech => $glosy) {
+        $content .= "$tech:$glosy\n";
+    }
+    file_put_contents('ankieta.txt', $content);
+}
+
+function czyZaznaczonoCheckbox($tech) {
+    foreach ($tech as $t) {
+        if (isset($_POST[$t])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (czyZaznaczonoCheckbox($tech)) {
+        $wyniki = wczytajWyniki($tech);
+
+        foreach ($tech as $t) {
+            if (isset($_POST[$t])) {
+                $wyniki[$t]++;
+            }
+        }
+
+        zapiszWyniki($wyniki);
+
+        echo "<h2>Wyniki ankiety:</h2>";
+        echo "<ul>";
+        foreach ($wyniki as $technologia => $glosy) {
+            echo "<li>$technologia - $glosy</li>";
+        }
+        echo "</ul>";
+        echo "<p><a href='ankieta.php'>Powrót do ankiety</a></p>";
+    } else {
+        echo "<h2>Musisz zaznaczyć przynajmniej jedną technologię!</h2>";
+        echo "<p><a href='ankieta.php'>Powrót do ankiety</a></p>";
+    }
+} else {
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Ankieta</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <h2>Wybierz technologie, które znasz:</h2>
+    <form method="POST">
+        <?php
+        foreach ($tech as $t) {
+            echo "<div>";
+            echo "<input type='checkbox' name='$t' id='$t'>";
+            echo "<label for='$t'>$t</label>";
+            echo "</div>";
+        }
+        ?>
+        <input type="submit" value="Wyślij">
+    </form>
+    </body>
+    </html>
+    <?php
+}
 ?>
